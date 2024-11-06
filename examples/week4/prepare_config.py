cea0881cc9940df7b67178e0f195bf38d2d150db
majklost <majkl.mrkos@seznam.cz>
2024-11-06 21:59:43 +0100

    tests done

--- examples/week4/prepare_config.py
+++ examples/week4/prepare_config.py
@@ -1,4 +1,4 @@
-from deform_plan.helpers.config_manager import ConfigManager,SUBSAMPLER,GOAL_BIAS
+from deform_plan.helpers.config_manager import ConfigManager, SUBSAMPLER, GOAL_BIAS
 
 
 seg_num = 50
@@ -6,7 +6,7 @@ conf = {
     'GUIDER_PERIOD': 10,
     'SEGMENT_NUM': seg_num,
     'MAX_FORCE_PER_SEGMENT': .1,
-    'ITERATIONS': 15000,
+    'ITERATIONS': 25000,
     'REACHED_THRESHOLD': 30,
     'CONTROL_IDXS': [i for i in range(seg_num)],
     'SAMPLING_PERIOD': 50,
@@ -23,12 +23,12 @@ conf = {
 }
 
 
-#prepare config to file
+# prepare config to file
 def prepare_vanilla(fpath):
-
 
     cm = ConfigManager(conf)
     cm.save_to_file(fpath)
+
 
 def prepare_goal_bias(fpath):
     cm = ConfigManager(conf)
@@ -39,13 +39,14 @@ def prepare_goal_bias(fpath):
     })
     cm.save_to_file(fpath)
 
+
 def prepare_subsampler(fpath):
     cm = ConfigManager(conf)
     cm.update(SUBSAMPLER)
     cm.save_to_file(fpath)
 
+
 if __name__ == "__main__":
     prepare_vanilla("vanilla_config.json")
     prepare_goal_bias("goal_bias_config.json")
     prepare_subsampler("subsampler_config.json")
-
